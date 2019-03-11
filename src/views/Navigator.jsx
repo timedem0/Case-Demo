bbeb06d196a9e9fe51b4772764f65b26f43b464d
Tudor <timedemo@gmail.com>
2019-03-11 22:39:28 +0200

    Formatting.

--- src/views/Navigator.jsx
+++ src/views/Navigator.jsx
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Navigator extends Component {
-	render() {
-		return (
-			<div>
-				<nav>
-					<Link to="/">Categories</Link>
-					-
-					<Link to="/categories_expensive">Expensive Categories</Link>
-				</nav>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <nav>
+          <Link to="/">Categories</Link>
+          -
+          <Link to="/categories_expensive">Expensive Categories</Link>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Navigator;
--- src/views/categoryViews/CategoriesExpensive.jsx
+++ src/views/categoryViews/CategoriesExpensive.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-// import CategoryAdd from '../../components/categoryComponents/CategoryAdd';
 import CategoryListExpensive from '../../components/categoryComponents/CategoryListExpensive';
-// import CategoryRandom from '../../components/categoryComponents/CategoryRandom';
 
 export default class Categories extends Component {
 
